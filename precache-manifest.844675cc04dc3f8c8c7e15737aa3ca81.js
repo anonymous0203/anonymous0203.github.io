2bfe4657be3320daa14ecb7f55de739b464edab9
anonymous <anonymous@example.com>
2021-02-11 17:04:31 -0800

    init

---
+++ precache-manifest.844675cc04dc3f8c8c7e15737aa3ca81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd931a6dd67555845a6cc265bd5b7011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c6ffbaca37b4c78f468",
+    "url": "/static/css/main.c24bf590.chunk.css"
+  },
+  {
+    "revision": "850253e4a2cab152036b",
+    "url": "/static/js/2.1455f374.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.1455f374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6ffbaca37b4c78f468",
+    "url": "/static/js/main.9e334c70.chunk.js"
+  },
+  {
+    "revision": "5517533e830a1b6f8d94",
+    "url": "/static/js/runtime-main.1654a17c.js"
+  }
+]);
